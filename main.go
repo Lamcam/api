92e8a8d39b2d5c2c8175584c1700352fbe32bac2
LamCam <lamcam813@gmail.com>
2022-12-24 18:58:10 +0700

    api go fiber

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Lamcam/api/database"
+	"github.com/Lamcam/api/routes"
+	"github.com/gofiber/fiber/v2"
+)
+
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("Welcome to my API")
+}
+
+func setupRoutes(app *fiber.App) {
+	// welcome endpoint
+	app.Get("/api", welcome)
+
+	//User endpoint
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+	app.Get("/api/users/:id", routes.GetUser)
+	app.Put("/api/users/:id", routes.UpdateUser)
+	app.Delete("/api/users/:id", routes.DeleteUser)
+
+	// Products endpoint
+	app.Post("/api/products", routes.CreateProduct)
+	app.Get("/api/products", routes.GetProducts)
+	app.Get("/api/products/:id", routes.GetProduct)
+	app.Put("/api/products/:id", routes.UpdateProduct)
+	app.Delete("/api/products/:id", routes.DeleteProduct)
+
+	// Order endpoint
+	app.Post("/api/orders", routes.CreateOrder)
+	app.Get("/api/orders", routes.GetOrders)
+	app.Get("/api/orders/:id", routes.GetOrder)
+
+}
+
+func main() {
+	database.ConnectDb()
+
+	app := fiber.New()
+
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(":3000"))
+}
